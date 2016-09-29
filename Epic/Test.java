983a5239d3c49fa5090dcf571c2f57ede2de6072
Jemma <lavender.wenwen@gmail.com>
2016-09-29 17:48:40 -0500

    Delete Test.java

--- Epic/Test.java
+++
@@ -1,9 +0,0 @@
-package Epic;
-
-public class Test {
-	public static final int INIT_SIZE = 500;
-	public static long methodCount[] = new long[INIT_SIZE];
-	public static void main(String[] args) {
-		System.out.println(methodCount.length);
-	}
-}
